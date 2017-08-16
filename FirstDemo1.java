7b4342ebc03a9d16c72c5945e3117cf650d519b0
IITDHN <30963668+IITDHN@users.noreply.github.com>
2017-08-16 23:10:58 +0530

    CODES1

---
+++ FirstDemo1.java
@@ -0,0 +1,17 @@
+public class FirstDemo
+{
+
+public static void main(String args[])
+{
+System.out.println(" hello java");
+}
+}
+
+class FirstDemo1
+{
+
+	public static void main(int a)
+	{
+		System.out.println(" hello java 1");
+	}
+}
